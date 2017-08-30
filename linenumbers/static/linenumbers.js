5364b76cdeea5b250a9c94b02816370de899c6b5
Wouter de Vries <wouter@oxyzen.nl>
2017-08-30 15:24:44 +0200

    Automatically patch the global configuration to enable lineNumbers

--- linenumbers/static/linenumbers.js
+++ linenumbers/static/linenumbers.js
@@ -9,6 +9,32 @@ define([
     })
   }
 
+  function areLineNumbersEnabled() {
+    var codeCells = getCodeCells()
+    for (var cellIdx in codeCells) {
+      if (!codeCells[cellIdx].code_mirror.getOption('lineNumbers')) {
+        return false
+      }
+    }
+    return true
+  }
+
+  function enableLineNumbers() {
+    const patch = {
+      CodeCell: {
+        cm_config: {
+          lineNumbers: true
+        }
+      }
+    }
+    Jupyter.notebook.config.update(patch)
+
+    // Also need to set it now, as the patched config does not apply immediately
+    $.each(getCodeCells(), function (index, cell) {
+      cell.code_mirror.setOption('lineNumbers', true)
+    })
+  }
+
   function setContinuousLineNumbering() {
     var last_line = 0
     $.each(getCodeCells(), function (index, item) {
@@ -25,6 +51,12 @@ define([
   }
 
   function load_ipython_extension() {
+    if (!areLineNumbersEnabled()) {
+      console.warn('jupyter-linenumbers extension enabled, but global lineNumbers are not enabled. Patching the ' +
+        'configuration...')
+      enableLineNumbers()
+    }
+
     setEventHandlers()
 
     events.on('create.Cell', function() {
